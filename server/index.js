252f910f68095cdf895b7d425fda056ad0d8b9f3
sujatha-m <jhanavi.bhushan@gmail.com>
2021-10-06 15:14:23 -0400

    deleted unwated to code in Client side react app and connected mongod DB Atlas database from server side

--- server/index.js
+++ server/index.js
@@ -1,4 +1,30 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from 'express'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+const app = express()
+
+app.use(bodyParser.json({ limit: '20mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '20mb', extended: true }))
+
+app.use(cors())
+
+const CONNECTION_URL =
+  'mongodb+srv://SujathaM-test:test@practice.aolfa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const PORT = process.env.PORT || 5000
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Coneection is established and running on port: ${PORT}`)
+    )
+  )
+  .catch(err => console.log(err.message))
+
+mongoose.set('useFindAndModify', false)
