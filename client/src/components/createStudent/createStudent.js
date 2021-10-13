1487c17beb39885ab65eb70f35780e0969f6e48d
sujatha-m <jhanavi.bhushan@gmail.com>
2021-10-13 15:30:46 -0400

    created react components from client side

--- client/src/components/createStudent/createStudent.js
+++ client/src/components/createStudent/createStudent.js
@@ -1,8 +1,24 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
+import Button from '@mui/material/Button'
+import axios from 'axios'
+//import useStyles from './styles.js'
 
-export default function BasicTextFields () {
+export default function CreateStudent () {
+  //const classes = useStyles()
+
+  const [student, setStudent] = useState({
+    regNo: 0,
+    studentName: '',
+    grade: '',
+    section: ''
+  })
+
+  const createStudent = () => {
+    axios.post('http://localhost:5000/students', student)
+  }
+
   return (
     <Box
       component='form'
@@ -13,7 +29,45 @@ export default function BasicTextFields () {
       autoComplete='off'
     >
       <h2>Create Student</h2>
-      <TextField id='outlined-basic' label='Outlined' variant='outlined' />
+      <TextField
+        id='outlined-basic'
+        label='Registration No'
+        variant='outlined'
+        value={student.regNo}
+        onChange={event => {
+          setStudent({ ...student, regNo: event.target.value })
+        }}
+      />
+      <TextField
+        id='outlined-basic'
+        label='Name'
+        variant='outlined'
+        value={student.studentName}
+        onChange={event => {
+          setStudent({ ...student, studentName: event.target.value })
+        }}
+      />
+      <TextField
+        id='outlined-basic'
+        label='Grade'
+        variant='outlined'
+        value={student.grade}
+        onChange={event => {
+          setStudent({ ...student, grade: event.target.value })
+        }}
+      />
+      <TextField
+        id='outlined-basic'
+        label='Section'
+        variant='outlined'
+        value={student.section}
+        onChange={event => {
+          setStudent({ ...student, section: event.target.value })
+        }}
+      />
+      <Button variant='contained' color='primary' onClick={createStudent}>
+        Create
+      </Button>
     </Box>
   )
 }
